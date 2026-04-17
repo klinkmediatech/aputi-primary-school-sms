a73f0e18f307355958e97c6a3333ecb56615e613
klinkmediatech <okelloict@gmail.com>
2026-04-17 09:14:10 +0300

    Create auth.js

---
+++ js/auth.js
@@ -0,0 +1,11 @@
+import { auth } from "./firebase.js";
+import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+window.login = function () {
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => window.location.href = "index.html")
+    .catch(e => document.getElementById("msg").innerText = e.message);
+};
